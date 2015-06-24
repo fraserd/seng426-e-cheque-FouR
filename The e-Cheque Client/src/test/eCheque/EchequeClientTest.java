bc45cf7087ae0872a85da378b0b515eaa7f1787c
Fraser DeLisle <fmd@uvic.ca>
2015-06-24 16:18:31 -0700

    add junit test file for echequeclient.java

---
+++ The e-Cheque Client/src/test/eCheque/EchequeClientTest.java
@@ -0,0 +1,145 @@
+package test.eCheque; 
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/** 
+* EchequeClient Tester. 
+* 
+* @author Fraser DeLisle
+* @since <pre>Jun 10, 2015</pre> 
+* @version 1.0 
+*/ 
+public class EchequeClientTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: RunClient() 
+* 
+*/ 
+@Test
+public void testRunClient() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: run() 
+* 
+*/ 
+@Test
+public void testRun() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+/** 
+* 
+* Method: ConnectToServer() 
+* 
+*/ 
+@Test
+public void testConnectToServer() throws Exception { 
+//TODO: Test goes here... 
+/* 
+try { 
+   Method method = EchequeClient.getClass().getMethod("ConnectToServer"); 
+   method.setAccessible(true); 
+   method.invoke(<Object>, <Parameters>); 
+} catch(NoSuchMethodException e) { 
+} catch(IllegalAccessException e) { 
+} catch(InvocationTargetException e) { 
+} 
+*/ 
+} 
+
+/** 
+* 
+* Method: getSocketStream() 
+* 
+*/ 
+@Test
+public void testGetSocketStream() throws Exception { 
+//TODO: Test goes here... 
+/* 
+try { 
+   Method method = EchequeClient.getClass().getMethod("getSocketStream"); 
+   method.setAccessible(true); 
+   method.invoke(<Object>, <Parameters>); 
+} catch(NoSuchMethodException e) { 
+} catch(IllegalAccessException e) { 
+} catch(InvocationTargetException e) { 
+} 
+*/ 
+} 
+
+/** 
+* 
+* Method: processConnection() 
+* 
+*/ 
+@Test
+public void testProcessConnection() throws Exception { 
+//TODO: Test goes here... 
+/* 
+try { 
+   Method method = EchequeClient.getClass().getMethod("processConnection"); 
+   method.setAccessible(true); 
+   method.invoke(<Object>, <Parameters>); 
+} catch(NoSuchMethodException e) { 
+} catch(IllegalAccessException e) { 
+} catch(InvocationTargetException e) { 
+} 
+*/ 
+} 
+
+/** 
+* 
+* Method: CloseConnection() 
+* 
+*/ 
+@Test
+public void testCloseConnection() throws Exception { 
+//TODO: Test goes here... 
+/* 
+try { 
+   Method method = EchequeClient.getClass().getMethod("CloseConnection"); 
+   method.setAccessible(true); 
+   method.invoke(<Object>, <Parameters>); 
+} catch(NoSuchMethodException e) { 
+} catch(IllegalAccessException e) { 
+} catch(InvocationTargetException e) { 
+} 
+*/ 
+} 
+
+/** 
+* 
+* Method: processBankConection() 
+* 
+*/ 
+@Test
+public void testProcessBankConection() throws Exception { 
+//TODO: Test goes here... 
+/* 
+try { 
+   Method method = EchequeClient.getClass().getMethod("processBankConection"); 
+   method.setAccessible(true); 
+   method.invoke(<Object>, <Parameters>); 
+} catch(NoSuchMethodException e) { 
+} catch(IllegalAccessException e) { 
+} catch(InvocationTargetException e) { 
+} 
+*/ 
+} 
+
+} 
