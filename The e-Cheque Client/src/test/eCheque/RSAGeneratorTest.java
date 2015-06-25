0c7adcfb86177ee571e991fa8a77914cad890a59
Fraser DeLisle <illdred@gmail.com>
2015-06-24 17:08:37 -0700

    added two junit test files.

---
+++ The e-Cheque Client/src/test/eCheque/RSAGeneratorTest.java
@@ -0,0 +1,35 @@
+package test.eCheque; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* RSAGenerator Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>Jun 24, 2015</pre> 
+* @version 1.0 
+*/ 
+public class RSAGeneratorTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: GenerateRSAKeys() 
+* 
+*/ 
+@Test
+public void testGenerateRSAKeys() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
