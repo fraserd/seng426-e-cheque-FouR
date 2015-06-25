0281f386e2cccb5ead10f330fb83138d569ab65c
lscholte <lscholte@uvic.ca>
2015-06-24 17:10:29 -0700

    Adds JUnit test files for DigitalCertificate and DigitalCertificateIO

---
+++ e-Cheque Bank Side/src/test/eCheque/DigitalCertificateIOTest.java
@@ -0,0 +1,14 @@
+package test.eCheque;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DigitalCertificateIOTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ e-Cheque Bank Side/src/test/eCheque/DigitalCertificateTest.java
@@ -0,0 +1,14 @@
+package test.eCheque;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DigitalCertificateTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
