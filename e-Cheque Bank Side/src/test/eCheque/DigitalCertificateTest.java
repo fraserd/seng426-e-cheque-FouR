4f859bb571483e09efc656d576f2f6dc6c759c9c
lscholte <lscholte@uvic.ca>
2015-07-06 23:03:28 -0700

    Implements test cases for methods in the DigitalCertificate class

--- e-Cheque Bank Side/src/test/eCheque/DigitalCertificateTest.java
+++ e-Cheque Bank Side/src/test/eCheque/DigitalCertificateTest.java
@@ -2,88 +2,177 @@ package test.eCheque;
 
 import static org.junit.Assert.*;
 
+import java.security.KeyPair;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
+
 import org.junit.Test;
+
+import eCheque.DigitalCertificate;
+import eCheque.RSAGenerator;
 
 public class DigitalCertificateTest {
 
 	@Test
-	public void testSetHolderName() {
-		fail("Not yet implemented");
+	public void testSetAndGetHolderName() {
+		
+		DigitalCertificate dc = new DigitalCertificate();
+		String holderName;
+		
+		assertNull(dc.getHolderName());
+		
+		holderName = "";
+		dc.setHolderName(holderName);
+		assertEquals(holderName, dc.getHolderName());
+		
+		holderName = "John Doe";
+		dc.setHolderName(holderName);
+		assertEquals(holderName, dc.getHolderName());
+		
+		dc.setHolderName(null);
+		assertNull(dc.getHolderName());
 	}
 	
 	@Test
-	public void testSetSubject() {
-		fail("Not yet implemented");
+	public void testSetAndGetSubject() {
+		
+		DigitalCertificate dc = new DigitalCertificate();
+		String subject;
+		
+		assertNull(dc.getSubject());
+		
+		subject = "";
+		dc.setSubject(subject);
+		assertEquals(subject, dc.getSubject());
+		
+		subject = "Something";
+		dc.setSubject(subject);
+		assertEquals(subject, dc.getSubject());
+		
+		dc.setSubject(null);
+		assertNull(dc.getSubject());
 	}
 	
 	@Test
-	public void testSetIssuer() {
-		fail("Not yet implemented");
+	public void testSetAndGetIssuer() {
+		
+		DigitalCertificate dc = new DigitalCertificate();
+		String issuer;
+		
+		assertNull(dc.getIssuer());
+		
+		issuer = "";
+		dc.setIssuer(issuer);
+		assertEquals(issuer, dc.getIssuer());
+		
+		issuer = "CertificateIssuer";
+		dc.setIssuer(issuer);
+		assertEquals(issuer, dc.getIssuer());
+		
+		dc.setIssuer(null);
+		assertNull(dc.getIssuer());
 	}
 	
 	@Test
-	public void testSetSerialNumber() {
-		fail("Not yet implemented");
+	public void testSetAndGetSerialNumber() {
+		
+		DigitalCertificate dc = new DigitalCertificate();
+		String serialNumber;
+		
+		assertNull(dc.getSerialNumber());
+		
+		serialNumber = "";
+		dc.setSerialNumber(serialNumber);
+		assertEquals(serialNumber, dc.getSerialNumber());
+		
+		serialNumber = "ABCD-2015";
+		dc.setSerialNumber(serialNumber);
+		assertEquals(serialNumber, dc.getSerialNumber());
+		
+		dc.setSerialNumber(null);
+		assertNull(dc.getSerialNumber());
 	}
 	
 	@Test
-	public void testSetValidFrom() {
-		fail("Not yet implemented");
+	public void testSetAndGetValidFrom() {
+		
+		DigitalCertificate dc = new DigitalCertificate();
+		String validFrom;
+		
+		assertNull(dc.getValidFrom());
+		
+		validFrom = "";
+		dc.setValidFrom(validFrom);
+		assertEquals(validFrom, dc.getValidFrom());
+		
+		validFrom = "xxx";
+		dc.setValidFrom(validFrom);
+		assertEquals(validFrom, dc.getValidFrom());	
+		
+		dc.setValidFrom(null);
+		assertNull(dc.getValidFrom());
 	}
 	
 	@Test
-	public void testSetValidTo() {
-		fail("Not yet implemented");
+	public void testSetAndGetValidTo() {
+		
+		DigitalCertificate dc = new DigitalCertificate();
+		String validTo;
+		
+		assertNull(dc.getValidTo());
+		
+		validTo = "";
+		dc.setValidTo(validTo);
+		assertEquals(validTo, dc.getValidTo());
+		
+		validTo = "yyy";
+		dc.setValidTo(validTo);
+		assertEquals(validTo, dc.getValidTo());
+		
+		dc.setValidTo(null);
+		assertNull(dc.getValidTo());
 	}
 	
 	@Test
-	public void testSetPublicKey() {
-		fail("Not yet implemented");
+	public void testSetAndGetPublicKey() {
+		
+		DigitalCertificate dc = new DigitalCertificate();
+		PublicKey pubKey = null;
+		
+		assertNull(dc.getpublicKey());
+		
+		try {
+			KeyPair rsaKeys = new RSAGenerator().GenerateRSAKeys();
+			pubKey = rsaKeys.getPublic();
+		} catch (NoSuchAlgorithmException e) {
+			fail("Testing of setter and getter for publicKey attribute is blocked by an issue in the RSAGenerator class: the GenerateRSAKeys() method is throwing an exception");
+		}
+		
+		dc.setPublicKey(pubKey);
+		assertEquals(pubKey, dc.getpublicKey());
+		
+		dc.setPublicKey(null);
+		assertNull(dc.getpublicKey());
 	}
 	
 	@Test
 	public void testSetIssuerSignature() {
-		fail("Not yet implemented");
-	}
 		
-	@Test
-	public void testGetHolderName() {
-		fail("Not yet implemented");
-	}
-	
-	@Test
-	public void testGetSubject() {
-		fail("Not yet implemented");
-	}
-	
-	@Test
-	public void testGetIssuer() {
-		fail("Not yet implemented");
-	}
-	
-	@Test
-	public void testGetSerialNumber() {
-		fail("Not yet implemented");
-	}
-	
-	@Test
-	public void testGetValidFrom() {
-		fail("Not yet implemented");
-	}
-	
-	@Test
-	public void testGetValidTo() {
-		fail("Not yet implemented");
-	}
-	
-	@Test
-	public void testGetpublicKey() {
-		fail("Not yet implemented");
-	}
-	
-	@Test
-	public void testGetIssuerSignature() {
-		fail("Not yet implemented");
+		DigitalCertificate dc = new DigitalCertificate();
+		byte[] issuerSignature;
+		
+		assertNull(dc.getIssuerSignature());
+		
+		issuerSignature = new byte[] { };
+		dc.setIssuerSignature(issuerSignature);
+		assertEquals(issuerSignature, dc.getIssuerSignature());
+		
+		issuerSignature = new byte[] {1,2,3,4,5};
+		dc.setIssuerSignature(issuerSignature);
+		assertEquals(issuerSignature, dc.getIssuerSignature());
+		
+		dc.setIssuerSignature(null);
+		assertNull(dc.getIssuerSignature());
 	}
 
 }
