523be290502de5527f497d813e1118e6feb42a9c
lscholte <lscholte@uvic.ca>
2015-07-06 21:53:25 -0700

    Adds stub methods

--- e-Cheque Bank Side/src/test/eCheque/DigitalCertificateTest.java
+++ e-Cheque Bank Side/src/test/eCheque/DigitalCertificateTest.java
@@ -7,7 +7,82 @@ import org.junit.Test;
 public class DigitalCertificateTest {
 
 	@Test
-	public void test() {
+	public void testSetHolderName() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testSetSubject() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testSetIssuer() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testSetSerialNumber() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testSetValidFrom() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testSetValidTo() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testSetPublicKey() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testSetIssuerSignature() {
+		fail("Not yet implemented");
+	}
+		
+	@Test
+	public void testGetHolderName() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testGetSubject() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testGetIssuer() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testGetSerialNumber() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testGetValidFrom() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testGetValidTo() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testGetpublicKey() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testGetIssuerSignature() {
 		fail("Not yet implemented");
 	}
 
